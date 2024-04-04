83cccdf29d32be9a0112d0c97a1c1f8b80486f18
aurorax-neo <15047150695@163.com>
2024-04-04 20:19:40 +0800

    chore: 添加 OPTIONS 回应、测试ping

--- router/router.go
+++ router/router.go
@@ -11,7 +11,8 @@ func SetRouter(router *gin.Engine) {
 
 	router.GET("/", Index)
 	router.GET("/ping", middleware.Ping)
-	v1Router := router.Group("/v1", middleware.V1Cors)
+	v1Router := router.Group("/v1")
+	v1Router.Use(middleware.V1Cors)
 	v1Router.Use(middleware.V1Request)
 	v1Router.Use(middleware.V1Response)
 	v1Router.Use(middleware.V1Auth)
